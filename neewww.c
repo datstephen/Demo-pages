c2e0527aac4fd35741d441670a18d788900b8a51
datstephen <60983794+datstephen@users.noreply.github.com>
2023-03-29 09:55:48 +0000

    Add files via upload

---
+++ neewww.c
@@ -0,0 +1,8 @@
+# include stdio.h
+
+int A = 5;
+int b = 4;
+
+int C = A + B;
+printf(C)
+
---
+++ nnnn.py
@@ -0,0 +1,37 @@
+#OPERATORs
+'''
+A = 1
+B = 2
+print(A + B)
+
+// (+ - * /)
+**
+
+>
+<
+>=
+=<
+==
+!=
+
+and 
+or 
+not
+
+++(increments) a = a + 1 
+--(decrements) b = b - 1
+'''
+#ninini!
+
+print("ABCD")
+
+'''
+A
+B
+C
+D
+
+alphabets = ("ABCD")
+for character in alphabets:
+    print(character)
+'''
